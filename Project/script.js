12b0b8eebaad71307bf7502faa7fcc0c6f035fcc
InfoElektronika <muhammadadytya1@gmail.com>
2025-01-23 15:05:04 +0700

    Delete Project directory

--- Project/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('search-input').addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {  // Cek jika tombol yang ditekan adalah 'Enter'
-        searchComponent();  // Panggil fungsi pencarian
-    }
-});
-
